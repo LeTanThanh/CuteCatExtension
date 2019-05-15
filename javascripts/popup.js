2c21daac9b413e487780ddab5f83f1e571b14dd0
LeTanThanh <tanthanhcr95@gmail.com>
2019-05-15 10:10:15 +0700

    add_click_event_load_new_image

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-  alert('WORK');
+  randomImage();
+
+  $('#js-img').on('click', function() {
+    randomImage();
+  });
+
+  function randomImage() {
+    $('#js-img').attr('src', '/images/loading.gif');
+
+    $.ajax({
+      url: 'https://api.giphy.com/v1/gifs/random',
+      type: 'GET',
+      dataType: 'JSON',
+      data: {
+        api_key: 'CquT5TApZFwM8KxqlEVCKJuAuFieI6Zt',
+        tag: 'cat'
+      },
+      success: function(response) {
+        var url = response['data']['images']['original']['url'];
+        $('#js-img').attr('src', url);
+      }
+    });
+  }
 });
