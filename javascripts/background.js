c7d1cd05171bf9492311f17f0b3223030831f4a0
LeTanThanh <tanthanhcr95@gmail.com>
2019-05-15 12:17:31 +0700

    add_click_event_and_preload_images

--- javascripts/background.js
+++ javascripts/background.js
@@ -1,6 +1,5 @@
 chrome.runtime.onInstalled.addListener(function() {
   var gifUrls = []
-  var offset = 0;
   var loaded = true;
 
   $.ajax({
@@ -10,7 +9,7 @@ chrome.runtime.onInstalled.addListener(function() {
     data: {
       api_key: 'CquT5TApZFwM8KxqlEVCKJuAuFieI6Zt',
       q: 'cat',
-      offset: offset
+      offset: 0
     },
     success: function(response) {
       var data = response['data']
@@ -18,17 +17,12 @@ chrome.runtime.onInstalled.addListener(function() {
         var url = data[i]['images']['original']['url'];
         gifUrls.push(url);
       }
-      offset = offset + 25;
 
       chrome.storage.sync.set({gifUrls: gifUrls}, function() {
         console.log('\nInitialized gifUrls: ');
         for (var i = 0; i < gifUrls.length; i++) {
           console.log(gifUrls[i]);
         }
-      });
-
-      chrome.storage.sync.set({offset: offset}, function() {
-        console.log('\nInitialized offset: ' + offset);
       });
 
       chrome.storage.sync.set({loaded: loaded}, function() {
--- javascripts/popup.js
+++ javascripts/popup.js
@@ -1,24 +1,58 @@
 $(document).ready(function(){
-  randomImage();
+  var gifUrls;
+  var loaded;
+  var offset = 25;
+  var index = 0;
+
+  showFirstUrl();
 
   $('#js-img').on('click', function() {
-    randomImage();
+    showNextUrl();
+
+    if (index !=0 && index % 10 == 0) {
+      loadMoreUrl();
+    }
   });
 
-  function randomImage() {
-    $('#js-img').attr('src', '/images/loading.gif');
+  function showFirstUrl() {
+    chrome.storage.sync.get(['gifUrls', 'loaded'], function(result) {
+      gifUrls = result['gifUrls'];
+      loaded = result['loaded'];
 
+      if (loaded) {
+        $('#js-img').attr('src', gifUrls[index]);
+        index = index + 1;
+      }
+    });
+  }
+
+  function showNextUrl() {
+    if (!loaded) {
+      showFirstUrl();
+    } else {
+      $('#js-img').attr('src', gifUrls[index]);
+      index = index + 1;
+    }
+  }
+
+  function loadMoreUrl() {
     $.ajax({
-      url: 'https://api.giphy.com/v1/gifs/random',
+      url: 'https://api.giphy.com/v1/gifs/search',
       type: 'GET',
       dataType: 'JSON',
       data: {
         api_key: 'CquT5TApZFwM8KxqlEVCKJuAuFieI6Zt',
-        tag: 'cat'
+        q: 'cat',
+        offset: offset
       },
       success: function(response) {
-        var url = response['data']['images']['original']['url'];
-        $('#js-img').attr('src', url);
+        var data = response['data']
+        for (var i = 0; i < data.length; i++) {
+          var url = data[i]['images']['original']['url'];
+          gifUrls.push(url);
+        }
+
+        offset = offset + 25;
       }
     });
   }
